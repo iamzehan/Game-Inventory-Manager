43d186408368d4ee512dcdf04118f0de6bd4028f
Zehan <zehan.kareem@gmail.com>
2025-12-30 00:46:46 +0600

    feat(iamzehan): complete add new game feature

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -4,3 +4,27 @@ exports.renderGames = async (req, res) => {
     const games = await db.getAllGames();
     res.render("games", {title: "Games", games});
 }
+
+exports.renderAddGames = async (req, res) => {
+    const developers = await db.getDevelopers();
+    const genres = await db.getGenre();
+    res.render("addNewGame",{title: "Add New Game", developers, genres});
+}
+
+exports.postAddNewGame = async (req, res) => {
+  try {
+    const { title, description, genre_ids = [], developer_ids = [] } = req.body;
+
+    // 1. Create game
+    const gameId = await db.createGame(title, description);
+
+    // 2. Associate genres & developers
+    await db.addGenres(gameId, genre_ids);
+    await db.addDevelopers(gameId, developer_ids);
+
+    res.redirect("/games");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+};
--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -3,5 +3,6 @@ const gamesRouter = Router();
 const controller = require('../controllers/gamesController');
 
 gamesRouter.get("/", controller.renderGames);
-
+gamesRouter.get("/add", controller.renderAddGames);
+gamesRouter.post("/add", controller.postAddNewGame);
 module.exports = gamesRouter;

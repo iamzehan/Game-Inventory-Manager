34b8491ead29d9d436b1e953b07ddc6e11fcd831
Zehan <zehan.kareem@gmail.com>
2025-12-30 19:49:26 +0600

    feat(iamzehan): add game update router

--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/gamesController');
 
 gamesRouter.get("/", controller.renderGames);
 gamesRouter.get("/{:id}/edit", controller.renderEditGames);
+gamesRouter.post("/{:id}/update", controller.postUpdateGame);
 gamesRouter.get("/add", controller.renderAddGames);
 gamesRouter.post("/add", controller.postAddNewGame);
 module.exports = gamesRouter;

8b9c5ca34ed5ff7cdb802f0e23846899e98a125b
Zehan <zehan.kareem@gmail.com>
2025-12-31 23:25:50 +0600

    feat(iamzehan): added names/titles to the delete message in the dialog

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,9 +2,12 @@ const deleteForm = document.querySelectorAll("form#delete-form");
 const dialog = document.body.querySelector("dialog.delete-dialog");
 const cancel = document.body.querySelector("button.cancel");
 const dialogDelete = dialog.querySelector(".dialog.delete");
+const dialogMessage = dialog.querySelector(".dialog.message");
 deleteForm.forEach((form) => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const name = e.target.getAttribute('data-id');
+    dialogMessage.textContent = name;
     dialog.showModal();
     dialogDelete.addEventListener("click", (e) => {
       form.submit();

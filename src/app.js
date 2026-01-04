8430ddb4b8cd5e29212fff51a57ccd90c75082f4
Zehan <zehan.kareem@gmail.com>
2026-01-04 20:18:31 +0600

    feat(iamzehan): all setup regarding session management and authentication added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 // environment
-require('dotenv').config()
+require("dotenv").config();
 
 // express
-const express = require('express');
+const express = require("express");
 const app = express();
 
-// path 
-const path = require('path');
+// session
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+// pg pool
+const pool = require("./models/pool");
+// flash to show error and success messages
+const flash = require("connect-flash");
+// csrf token to add security in forms
+const csrf = require("csurf");
+
+// passport
+const passport = require("./auth/passport");
+
+// path
+const path = require("path");
+
+// static files setup
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // // Create livereload server watching views & public
 if (process.env.NODE_ENV !== "production") {
@@ -18,7 +39,7 @@ if (process.env.NODE_ENV !== "production") {
   liveReloadServer.watch([
     path.join(__dirname, "views/*.ejs"),
     path.join(__dirname, "public/css"),
-    path.join(__dirname, "public/js")
+    path.join(__dirname, "public/js"),
   ]);
 
   // Inject livereload script into Express
@@ -29,34 +50,67 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
-// middlewares
+// middlewares for request body parsing
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// static files setup
-const assetsPath = path.join(__dirname, "public")
-app.use(express.static(assetsPath))
+// Session configuration
+app.use(
+  session({
+    store: new pgSession({
+      pool: pool,
+      tableName: "session",
+    }),
+    name: "sid",
+    secret: process.env.SESSION_SECRET || "dev-secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days,
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    },
+  })
+);
+// passport configuration
+app.use(passport.initialize());
+app.use(passport.session());
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+// flash messages configuration
+app.use(flash());
+
+// csrf configuration
+app.use(csrf());
+
+// Global Template variables
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.csrfToken = req.csrfToken();
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
 
 // routes
-const routes = require('./routes');
+const routes = require("./routes");
+const { ensureAuth, ensureGuest } = require("./controllers/authController");
 
-app.use("/", routes.dashboardRouter);
-app.use("/games", routes.gamesRouter);
-app.use("/genres", routes.genreRouter);
-app.use("/developers", routes.developersRouter);
+app.use("/login", ensureGuest, routes.loginRouter);
+app.use("/logout", ensureAuth, routes.logoutRouter);
 
+app.use("/", ensureAuth, routes.dashboardRouter);
+app.use("/games", ensureAuth, routes.gamesRouter);
+app.use("/genres", ensureAuth, routes.genreRouter);
+app.use("/developers", ensureAuth, routes.developersRouter);
 
 // port
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || "3000";
 
 // listen
-app.listen(PORT, (err)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log(`Running server on: http://localhost:${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Running server on: http://localhost:${PORT}`);
 });

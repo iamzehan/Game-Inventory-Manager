8b12008ae835718782186a514564ec4f63eec2c5
Zehan <zehan.kareem@gmail.com>
2026-01-04 18:45:58 +0600

    feat(iamzehan): added Authentication queries

--- src/models/queries.js
+++ src/models/queries.js
@@ -269,3 +269,23 @@ exports.getRandomGames = async () => {
   const {rows} = await pool.query(SQL);
   return rows;
 }
+
+// authentication queries
+
+exports.createUser = async (username, password) => {
+  const SQL = "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *";
+  const {rows} = await pool.query(SQL, [username, password]);
+  return rows;
+};
+
+exports.getUser = async (username) => {
+  const SQL = `SELECT * FROM users WHERE username = $1`
+  const {rows} = await pool.query(SQL, [username]);
+  return rows[0];
+}
+
+exports.getUserById = async (id) => {
+  const SQL = `SELECT * FROM users WHERE id=$1`
+  const {rows} = await pool.query(SQL, [id]);
+  return rows[0];
+}
